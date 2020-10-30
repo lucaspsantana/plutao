ac5e244585d9dd56f8bf909d7de05bf3669a8041
Lucas Santana <lanatnas36@gmail.com>
2020-10-30 00:48:02 -0300

    bullet

--- src/Objects/Bullet.gd
+++ src/Objects/Bullet.gd
@@ -20,3 +20,7 @@ func _on_VisibilityNotifier2D_screen_exited():
 func _on_Bullet_body_entered(body: Node) -> void:
 	if(body != actor):
 		queue_free()
+
+
+func _on_Bullet_area_entered(area):
+	queue_free()
