a9c19f7c4cc85bdc89829a610d425ce6490a6960
Filipe Fiusa <flafigol@gmail.com>
2020-10-30 01:02:55 -0300

    Added 3 waves of enemies

--- src/Actors/Enemy1.gd
+++ src/Actors/Enemy1.gd
@@ -3,7 +3,10 @@ extends Enemy
 func _ready() -> void:
 	guns.append(get_node("GunBarrel"))
 	
-	life = 5
+	life = 3
 	speed = 100
-	attack_speed = 2
+	attack_speed = 3
 	velocity = Vector2(0,1)
+
+
+
