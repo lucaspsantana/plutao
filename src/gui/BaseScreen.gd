1094f766b18ecf1e2e7e2c99d0840c89bb7658f8
Lucas Santana <lanatnas36@gmail.com>
2020-11-04 23:52:48 -0300

    resolução da tela

--- src/gui/BaseScreen.gd
+++ src/gui/BaseScreen.gd
@@ -3,8 +3,8 @@ extends CanvasLayer
 onready var tween = $Tween
 
 func appear():
-	tween.interpolate_property(self, "offset:x", 500, 0, 0.5, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
+	tween.interpolate_property(self, "offset:x", 720, 0, 0.5, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
 	tween.start()
 func disappear():
-	tween.interpolate_property(self, "offset:x", 0, 500, 0.4, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
+	tween.interpolate_property(self, "offset:x", 0, 720, 0.4, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
 	tween.start()
