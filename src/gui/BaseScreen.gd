412f56529bc07274de4ff4fdd2434df8137e7f5f
Lucas Santana <lanatnas36@gmail.com>
2020-11-06 00:30:08 -0300

    animation

--- src/gui/BaseScreen.gd
+++ src/gui/BaseScreen.gd
@@ -3,8 +3,8 @@ extends CanvasLayer
 onready var tween = $Tween
 
 func appear():
-	tween.interpolate_property(self, "offset:x", 720, 0, 0.5, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
+	tween.interpolate_property(self, "offset:x", 600, 0, 0.5, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
 	tween.start()
 func disappear():
-	tween.interpolate_property(self, "offset:x", 0, 720, 0.4, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
+	tween.interpolate_property(self, "offset:x", 0, 600, 0.4, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
 	tween.start()
