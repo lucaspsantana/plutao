931ae270d353c2a8cfa20245fc43d64c9f7f87c1
Filipe Fiusa <flafigol@gmail.com>
2020-11-06 21:18:34 -0300

    Boss path bug fix

--- src/Paths/BossPath.gd
+++ src/Paths/BossPath.gd
@@ -5,14 +5,12 @@ var onscreen = false
 func _process(delta: float) -> void:
 	if(!onscreen):
 		position.y += 100 * delta
-		return
-	
-	$PathFollow2D/Boss.onscreen = true
+		return		
 
 
 func _on_VisibilityNotifier2D_viewport_entered(viewport: Viewport) -> void:
 	onscreen = true
-
+	$PathFollow2D/Boss.onscreen = true
 
 func _on_VisibilityNotifier2D_screen_entered() -> void:
 	onscreen = true
